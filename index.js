c631fb7172fe74edb8866e5eef0689089ffd82e8
Paulo <pauloxti@gmail.com>
2023-02-10 19:52:44 +0100

    fix list

--- index.js
+++ index.js
@@ -12,9 +12,13 @@ const cors = require('cors');
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = 3001;
-let addresses = [
-    "0x31935883802D258D73d698AA2aaB31cCE308DBF0",
-    "0xCF49661e783c2b7Bf581106c9f88FFA765752e3d"
+let addresses1 = [
+    "0xCF49661e783c2b7Bf581106c9f88FFA765752e3d",
+    "0x31935883802D258D73d698AA2aaB31cCE308DBF0"
+];
+let addresses2 = [
+    "0xE5ce1Bc690a23780c292fEeB0c734FDbC29dC754",
+    "0xd7B420916B543Aed6926523424CA16C63e948A15"
 ];
 let rowCounter = 0;
 /*
@@ -31,19 +35,31 @@ while (line = liner.next()) {
   })
 }
 */
-const merkleTree = new merkletreejs_1.MerkleTree(addresses, keccak256_1.default, { hashLeaves: true, sortPairs: true });
+const merkleTree1 = new merkletreejs_1.MerkleTree(addresses1, keccak256_1.default, { hashLeaves: true, sortPairs: true });
+const merkleTree2 = new merkletreejs_1.MerkleTree(addresses2, keccak256_1.default, { hashLeaves: true, sortPairs: true });
 app.use(cors({
     //origin: 'https:website.com'
     origin: '*'
 }));
 app.use(express_1.default.static('public'));
-console.log("root", merkleTree.getRoot().toString('hex'));
+console.log("root1", merkleTree1.getRoot().toString('hex'));
+console.log("root2", merkleTree2.getRoot().toString('hex'));
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
 });
-app.get('/get_proof_by_address/:address', (req, res) => {
+app.get('/get_proof_by_address/:list/:address', (req, res) => {
     let address = req.params.address;
-    const proof = merkleTree.getHexProof((0, keccak256_1.default)(address));
+    let list = req.params.list;
+    let proof;
+    if (list === "1") {
+        proof = merkleTree1.getHexProof((0, keccak256_1.default)(address));
+    }
+    else if (list === "2") {
+        proof = merkleTree2.getHexProof((0, keccak256_1.default)(address));
+    }
+    else {
+        proof = [];
+    }
     res.send(proof);
 });
 app.listen(port, () => {
--- index.ts
+++ index.ts
@@ -10,10 +10,16 @@ dotenv.config();
 const app: Express = express();
 const port = 3001;
 
-let addresses: string[] = [
-  "0x31935883802D258D73d698AA2aaB31cCE308DBF0",
-  "0xCF49661e783c2b7Bf581106c9f88FFA765752e3d"
+let addresses1: string[] = [
+  "0xCF49661e783c2b7Bf581106c9f88FFA765752e3d",
+  "0x31935883802D258D73d698AA2aaB31cCE308DBF0"
 ]
+
+let addresses2: string[] = [
+  "0xE5ce1Bc690a23780c292fEeB0c734FDbC29dC754",
+  "0xd7B420916B543Aed6926523424CA16C63e948A15"
+]
+
 let rowCounter = 0;
 
 /*
@@ -31,8 +37,14 @@ while (line = liner.next()) {
 }
 */
 
-const merkleTree = new MerkleTree(
-  addresses,
+const merkleTree1 = new MerkleTree(
+  addresses1,
+  keccak256,
+  { hashLeaves: true, sortPairs: true }
+)
+
+const merkleTree2 = new MerkleTree(
+  addresses2,
   keccak256,
   { hashLeaves: true, sortPairs: true }
 )
@@ -44,14 +56,25 @@ app.use(cors({
 
 app.use(express.static('public'))
 
-console.log("root", merkleTree.getRoot().toString('hex'));
+console.log("root1", merkleTree1.getRoot().toString('hex'));
+console.log("root2", merkleTree2.getRoot().toString('hex'));
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
-app.get('/get_proof_by_address/:address', (req: Request, res: Response) => {
+app.get('/get_proof_by_address/:list/:address', (req: Request, res: Response) => {
   let address: string = req.params.address;
-  const proof = merkleTree.getHexProof(keccak256(address));
+  let list: string = req.params.list;
+  let proof: string[];
+
+  if (list === "1") {
+     proof = merkleTree1.getHexProof(keccak256(address));
+  } else if (list === "2") {
+     proof = merkleTree2.getHexProof(keccak256(address));
+  } else {
+     proof = [];
+  }
+
   res.send(proof);
 });
 
